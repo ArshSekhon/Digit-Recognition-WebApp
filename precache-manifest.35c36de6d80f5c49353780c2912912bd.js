a230ac0d21a2c1272095fa851b6a82a20738a104
Arsh Sekhon <>
2018-11-02 12:50:51 -0700

    Updates

---
+++ precache-manifest.35c36de6d80f5c49353780c2912912bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Digit-Recognition-WebApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b33897f6374484ad3ae0",
+    "url": "/Digit-Recognition-WebApp/static/js/runtime~main.b33897f6.js"
+  },
+  {
+    "revision": "496dece0b3be56769a24",
+    "url": "/Digit-Recognition-WebApp/static/js/main.496dece0.chunk.js"
+  },
+  {
+    "revision": "53c5681b13bff4fb34f1",
+    "url": "/Digit-Recognition-WebApp/static/js/1.53c5681b.chunk.js"
+  },
+  {
+    "revision": "496dece0b3be56769a24",
+    "url": "/Digit-Recognition-WebApp/static/css/main.fb04d602.chunk.css"
+  },
+  {
+    "revision": "53c5681b13bff4fb34f1",
+    "url": "/Digit-Recognition-WebApp/static/css/1.926c42c2.chunk.css"
+  },
+  {
+    "revision": "1fda212de667df2def1983ea1156aa62",
+    "url": "/Digit-Recognition-WebApp/index.html"
+  }
+];
